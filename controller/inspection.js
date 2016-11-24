22242d7c36c1493c737bda7552a5a9d117981021
Ruan Martinelli <martinelliruan@gmail.com>
2016-11-24 00:52:47 -0200

    return bundles in endpoint

--- controller/inspection.js
+++ controller/inspection.js
@@ -1,13 +1,21 @@
-const { inspections } = require('../data');
+const { inspections, bundles } = require('../data');
 
 module.exports = (app) => {
 
-    app.get('/api/inspection', (req, res, next) => res.send(inspections));
+    app.get('/api/inspection', (req, res, next) => {
+        console.log("/get inspections");
+    
+        res.send(inspections)
+    });
 
     app.get('/api/inspection/:id', (req, res, next) => {
         const id = req.params.id;
 
-        res.send(inspections.filter(insp => id == insp.id));
+        console.log('/get inspection/' + id);
+        let data = inspections.filter(insp => id == insp.id)[0];
+        data.bundles = bundles.filter(bdl => bdl.id_inspection == id);
+        console.log(data);
+        res.send(data);
     });
 
     app.post('/api/inspection/', (req, res, next) => {
